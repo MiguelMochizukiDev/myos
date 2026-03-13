99142643c92daaa3c5e33c6148a2e20f5670058e
MiguelMochizukiDev <miguelmochizukisilva@gmail.com>
2026-03-12 21:55:37 -0300

    boot: add Multiboot-compliant assembly entry point

---
+++ boot/boot.asm
@@ -0,0 +1,24 @@
+; boot.asm
+MBOOT_MAGIC     equ 0x1BADB002
+MBOOT_FLAGS     equ 0x0
+MBOOT_CHECKSUM  equ -(MBOOT_MAGIC + MBOOT_FLAGS)
+
+section .multiboot
+align 4
+    dd MBOOT_MAGIC
+    dd MBOOT_FLAGS
+    dd MBOOT_CHECKSUM
+
+section .text
+extern kernel_main
+global _start
+_start:
+    mov esp, stack_top
+    call kernel_main
+    hlt
+
+section .bss
+stack_bottom:
+    resb 16384
+stack_top:
+    
