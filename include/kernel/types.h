6a7636d71fe31ec18ab41eb8e0a8a1294ee8245f
MiguelMochizukiDev <miguelmochizukisilva@gmail.com>
2026-03-23 20:04:42 -0300

    feat: add type aliases for kernel development
    - Define our own fixed-width types matching x86-64 LP64 model

---
+++ include/kernel/types.h
@@ -0,0 +1,33 @@
+/**
+ * types.h
+ * Fundamental kernel type aliases
+ *
+ * Necessary because we are not linking against any libc.
+ * Notice we follow x86-64 LP64 model.
+ * These must be included before any other kernel header.
+ */
+
+#ifndef KERNEL_TYPES_H
+#define KERNEL_TYPES_H
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+typedef signed long long int64_t;
+
+typedef uint64_t uintptr_t;
+typedef int64_t intptr_t;
+typedef uint64_t size_t;
+
+typedef _Bool bool;
+#define true 1;
+#define false 0;
+
+#define NULL ((void *)0)
+
+#endif
